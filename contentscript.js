9050ea3c7f83702dd3bdc5b038bad362114c135d
Brian Ryckbost <bryckbost@gmail.com>
2010-04-01 08:14:27 -0400

    Still trying to get Sumo to work. Stupid parse errors

--- contentscript.js
+++ contentscript.js
@@ -1,18 +1,3 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// The possible log levels.
-var logLevels = {
-    "none": 0,
-    "error": 1,
-    "info": 2
-};
-
-// Defines the current log level. Values other than "none" are for debugging
-// only and should at no point be checked in.
-var currentLogLevel = logLevels.info;
-
 findMicroformats();
 
 function findMicroformats() {
