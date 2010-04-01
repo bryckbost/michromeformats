befedeb13f0c960920a584ef5513e8ec9093c969
Brian Ryckbost <bryckbost@gmail.com>
2010-04-01 08:50:48 -0400

    I should really just get the hcards working first before I start adding in support for hcalendars and hreviews.

--- contentscript.js
+++ contentscript.js
@@ -2,10 +2,13 @@ findMicroformats();
 
 function findMicroformats() {
 	var hCards = HCard.discover();
-	for(i = 0; i < hCards.length; i++) {
-		hCards[i].n = JSON.stringify(hCards[i].n);
-		hCards[i] = JSON.stringify(hCards[i]);
-	}
-	if(hCards.length > 0)
+	var hCalendars = HCalendar.discover();
+	var hReviews = HReview.discover();
+	
+	if(hCards.length > 0 || hCalendars.length > 0 || hReviews.length > 0) {
 		chrome.extension.sendRequest({hCards: hCards});
+		chrome.extension.sendRequest({hCalendars: hCalendars});
+		chrome.extension.sendRequest({hReviews: hReviews});
+	}
+
 }
