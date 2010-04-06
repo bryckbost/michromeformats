0877c6abae41cbb9b33c231de6a217e5348c4e59
Brian Ryckbost <bryckbost@gmail.com>
2010-04-06 08:42:23 -0400

    Playing around with message passing

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,18 @@
-discover_microformats();
+// The background page is asking us to find a microformat on the page.
+if (window == top) {
+  chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
+    sendResponse(discover_microformats());
+  });
+}
 
-function discover_microformats() {
+var discover_microformats = function() {
 	var hcards = HCard.discover();
   var hcalendars = HCalendar.discover();
   var hreviews = HReview.discover();
-	
-	if(hcards.length > 0 || hcalendars.length > 0 || hreviews.length > 0) {
-		chrome.extension.sendRequest({hcards: hcards});
-    chrome.extension.sendRequest({hcalendars: hcalendars});
-    chrome.extension.sendRequest({hreviews: hreviews});
-	}
+  var microformats = new Array(hcards, hcalendars, hreviews);
+  
+  console.log('hcards: ' + hcards.length);
+  console.log('hcalendars: ' + hcalendars.length);
+  console.log('hreviews: ' + hreviews.length);
+  console.log(microformats);
 }
