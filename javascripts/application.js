d95596b147107b08dd1cfa9b323b28e6e465e7b2
Brian Ryckbost <bryckbost@gmail.com>
2010-06-04 15:31:57 -0400

    Cleaning up the popup.html. Add application.js and popup.css

---
+++ javascripts/application.js
@@ -0,0 +1,19 @@
+function unique_cards(array) {
+  var a = [], l = array.length;
+  for(var i = 0; i < l; i++) {
+      for(var j = i+1; j < l; j++)
+          if (array[i].fn === array[j].fn) j = ++i;
+      a.push(array[i]);
+  }
+  return a;
+};
+
+function unique_events(array) {
+  var a = [], l = array.length;
+  for(var i = 0; i < l; i++) {
+      for(var j = i+1; j < l; j++)
+          if (array[i].summary === array[j].summary && array[i].dtstart === array[j].dtstart) j = ++i;
+      a.push(array[i]);
+  }
+  return a;
+};
